69f5364d34ef0b8392625b455c479064796c0e55
root <root@DESKTOP-R66B3GE.localdomain>
2026-04-28 08:16:49 +0000

    third function

---
+++ Docker_zero_to_hero/example/app.py
@@ -0,0 +1,1 @@
+print("Hello , This is my first Python Program as !!!!!!HELLO WORLD!!!!!!")
---
+++ cal.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "calculations for two numbers :"
+x=a+b
+print ($x);
+echo "subtraction")
+y=a-b;
+print ($y);
+
